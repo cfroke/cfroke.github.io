4c2d6251b4d1f6095d68530805cc54775e4c412e
Casey <caseyfroke@gmail.com>
2020-04-27 16:50:51 -0600

    updated site

---
+++ src/components/about.jsx
@@ -0,0 +1,108 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Me</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>I am a software engineer and scrum master from Falcon, Colorado.I love exploring new technologies and am often amazed by the progress we as a human species have made so far.</p>
+                    <p>I have started playing around with react again lately and decided to build this site. </p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">Here are some things I have experience with</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Web Development </h3>
+                    <p>I have experience building websites using JavaScript,React,HTML,CSS</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Radar Systems</h3>
+                    <p>In my current position I am working on the next generation radar</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Dev Ops</h3>
+                    <p>I work with tools like Jenkins, Fisheye, Crucible</p>
+                </div>
+                </div>
+            </div>
+            
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-2">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Agile</h3>
+                    <p>In my current position I am a scrum master and use tools like Jira</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-4">
+                <span className="icon">
+                    <i className="icon-layers2" />
+                </span>
+                <div className="desc">
+                    <h3>Graphic Design</h3>
+                    <p>I offer Logo and website design</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-6">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Project Management</h3>
+                    <p>I have led many Software teams through all development stages.</p>
+                </div>
+                </div>
+            </div>
+            
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
