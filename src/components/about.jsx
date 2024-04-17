30068622019805ac159b4711e30c8ac899abe6d1
Casey <caseyfroke@gmail.com>
2024-04-16 22:21:15 -0600

    Adding updated files

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,8 +13,7 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">About Me</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a software engineer and scrum master from Falcon, Colorado.I love exploring new technologies and am often amazed by the progress we as a human species have made so far.</p>
-                    <p>I have started playing around with react again lately and decided to build this site. </p>
+                    <p>I am a Sr. Software Engineering Manager from Falcon, Colorado.I love exploring new technologies and am often amazed by the progress we as a human species have made so far. I am a self starter that works well in both independent and team environments. Fast paced and dynamic environments are where I thrive. I greatly enjoy helping others build out their career plans as well as help to develop engineering teams.</p>
                     </div>
                 </div>
                 </div>
@@ -34,33 +33,44 @@ export default class About extends Component {
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-1">
                 <span className="icon">
-                    <i className="icon-bulb" />
+                    <i className="icon-user" />
+                </span>
+                <div className="desc">
+                    <h3>People Management </h3>
+                    <p>In my current position I manage a software department that includes 23 managers and 350 engineers across multiple programs.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-2">
+                <span className="icon">
+                    <i className="icon-html-five2" />
                 </span>
                 <div className="desc">
                     <h3>Web Development </h3>
-                    <p>I have experience building websites using JavaScript,React,HTML,CSS</p>
+                    <p>I have experience building websites using JavaScript,React,HTML,CSS.</p>
                 </div>
                 </div>
             </div>
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-3">
                 <span className="icon">
-                    <i className="icon-phone3" />
+                    <i className="icon-world" />
                 </span>
                 <div className="desc">
                     <h3>Radar Systems</h3>
-                    <p>In my current position I am working on the next generation radar</p>
+                    <p>In my current position I am working on the next generation radar.</p>
                 </div>
                 </div>
             </div>
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-5">
                 <span className="icon">
-                    <i className="icon-data" />
+                    <i className="icon-git" />
                 </span>
                 <div className="desc">
                     <h3>Dev Ops</h3>
-                    <p>I work with tools like Jenkins, Fisheye, Crucible</p>
+                    <p>I use tools like Jenkins, Fisheye, Crucible, Bitbucket, Git. I have had some experience using Fortify.</p>
                 </div>
                 </div>
             </div>
@@ -72,33 +82,32 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Agile</h3>
-                    <p>In my current position I am a scrum master and use tools like Jira</p>
+                    <p>I have held positions as Product Owner, Scrum Master, as well as being a stakeholder within the Agile and Scrum methodologies.</p>
                 </div>
                 </div>
             </div>
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-4">
                 <span className="icon">
-                    <i className="icon-layers2" />
+                    <i className="icon-scissors" />
                 </span>
                 <div className="desc">
                     <h3>Graphic Design</h3>
-                    <p>I offer Logo and website design</p>
+                    <p>My minor in college was Graphic Information Technology and I hold a certification in Computer Graphic Design. I have designed multiple front end solutions for software projects in my current company and like to play around with web design as a hobby.</p>
                 </div>
                 </div>
             </div>
             <div className="col-md-4 text-center animate-box">
-                <div className="services color-6">
+                <div className="services color-9">
                 <span className="icon">
-                    <i className="icon-phone3" />
+                    <i className="icon-user" />
                 </span>
                 <div className="desc">
                     <h3>Project Management</h3>
-                    <p>I have led many Software teams through all development stages.</p>
+                    <p>I have led many Software teams through all stages of the project life-cycle as well as being the Project Manager for the environmental test lab in Rancho Bernardo, CA for Northrop Grumman.</p>
                 </div>
                 </div>
             </div>
-            
             </div>
         </div>
         </section>
--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Hi! <br />I'm Casey</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://cfroke.github.io/Casey Froke Resume.pdf" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="https://cfroke.github.io/Casey Froke Resume.docx" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
