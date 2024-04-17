37f950d645df24db8252394fca8c94e83c90572a
Casey <caseyfroke@gmail.com>
2024-04-16 22:35:53 -0600

    new updates

---
+++ precache-manifest.8ec0b9f674117fd3d12bd45c8f876d76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "759d9de3cfbb09bacf71b23aed68995c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "817c7ab215b914b2c03e",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "863641e1e29f95c86be1",
+    "url": "/static/js/2.121eca90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.121eca90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "817c7ab215b914b2c03e",
+    "url": "/static/js/main.08cf8c1c.chunk.js"
+  },
+  {
+    "revision": "3f206b418c45caa3e3bb",
+    "url": "/static/js/runtime-main.519cd7d6.js"
+  }
+]);
